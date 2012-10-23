896f2f6e1c1a9bd26fc51ef0108829fd68dca8a5
markxue2002 <markxue2002@gmail.com>
2012-10-22 23:23:10 -0500

    updated comments

--- js/event.js
+++ js/event.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 Parse.initialize("vd4AGWyFCpawPBI9o2hugn1YmRy1q0vV3aLSd3gr", "E7KcLQkrYLrccAcHGNamDkzXKDOqzfkQYoSdg7TJ");
+var commentStart = 0;
 
-var eventArray = new Array();
 
 function refreshPage() {
   $.mobile.changePage(
@@ -31,7 +31,7 @@ e.save({
 	}, {
   		success: function(e) {
 			
-			$("#eventlist").prepend("<li id="+String(e.id)+"><a href="+"eventDetails"+">"+e.get("Event")+"</a><span class="+"ui-li-count"+">"+ e.get("Attendees") +"</span><a onClick= incAttendees(this.parentNode.id);>Join</a></li>");
+			$("#eventlist").prepend("<li id="+String(e.id)+"><a href="+"javascript:void(0)"+" onClick= retreiveEventDetails(this.parentNode.parentNode.parentNode.id);>"+e.get("Event")+"</a><span class="+"ui-li-count"+">"+ e.get("Attendees") +"</span><a onClick= incAttendees(this.parentNode.id);>Join</a></li>");
 		
 	
 		$("#eventlist").listview("refresh");
@@ -66,12 +66,11 @@ query.find({
 		e = eventArray[i];
 		//alert( String(e.id));
 			
-        $("#eventlist").append("<li id="+String(e.id)+"><a href="+"eventDetails"+">"+e.get("Event")+"</a><span class="+"ui-li-count"+">"+ e.get("Attendees") +"</span><a onClick= incAttendees(this.parentNode.id);>Join</a></li>");
+        $("#eventlist").append("<li id="+String(e.id)+"><a onClick= retreiveEventDetails(this.parentNode.parentNode.parentNode.id);>"+e.get("Event")+"</a><span class="+"ui-li-count"+">"+ e.get("Attendees") +"</span><a onClick= incAttendees(this.parentNode.id);>Join</a></li>");
 		
 	
 		$("#eventlist").listview("refresh");
-		
-		
+	
 		}
 	
   },
@@ -97,6 +96,7 @@ e.increment("Attendees");
 e.save();
 $("#eventlist").listview("refresh");
 refreshPage();
+//$.mobile.pageLoading(true);
   },
   error: function(E, error) {
     // The object was not retrieved successfully.
@@ -108,19 +108,130 @@ refreshPage();
 }
 
 
-$(document).delegate('#eventDetails', 'pageshow', function () {
-    //Your code for each page load here
-	retreiveEventDetails();
+
+function retreiveEventDetails(EventID)
+{
+
+$.mobile.changePage("#eventDetails");
+//alert(EventID);
+// get Title and creator from Database
+var e = Parse.Object.extend("Event");
+var query = new Parse.Query(e);
+//alert(EventID);
+query.get(EventID, {
+  success: function(e) {
+// The object was retrieved successfully.
+
+//get specific info on object
+document.getElementById("eventTitle").innerHTML= e.get("Event");
+document.getElementById("eventCreator").innerHTML ="";
+//$("#eventDetails").listview("refresh");
+//refreshPage();
+//$.mobile.pageLoading(true);
+  },
+  error: function(E, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and description.
+	alert("there was an error retreiving object");
+  }
 });
 
 
-function retreiveEventDetails()
+//// retreive comments for each event
+var query = new Parse.Query("Comment");
+	//array that stores list of event objects
+    var commentArray = new Array();
+	query.ascending("createdAt");
+	query.equalTo("EventId", EventID);
+query.find({
+	success: function(results) {
+    	// results is an array of Parse.Object.
+		//alert(EventID);
+		//alert(results);
+		
+    	commentArray = results;
+		var len = commentArray.length;
+	    var c;		
+		
+		//clear the current list of comments 
+		$('#commentslist').children().remove('li');
+		
+	//iterate through and display the objects
+	for(i = 0; i < len; i++) {
+		c = commentArray[i];
+		//alert( String(e.id));
+		
+       $("#commentslist").append("<li id="+String(c.id)+"><h3>"+ c.get("comment")+"</h3><p>"+ "" +"</p><p class="+"ui-li-aside"+">"+ c.createdAt +"</p></li>");
+	
+		$("#commentslist").listview("refresh");
+		
+		}
+  },
+
+  error: function(error) {
+    	// error is an instance of Parse.Error.
+  }
+});	
+
+
+
+
+	
+}
+
+function newComment()
 {
-alert("showcomments");
-// get Title and creator
+	
+	var EventID;
+	var eventTitle = document.getElementById("eventTitle").innerHTML;
+	alert (eventTitle);
+	//look for event Id 
+var e = Parse.Object.extend("Event");
+var query = new Parse.Query(e);
+query.equalTo("Event",eventTitle );
+query.first({
+  success: function(e) {
+// The object was retrieved successfully.
 
-// get comments
+//get specific info on object
+EventID = e.id;
+//alert("event Id ="+ EventID);
 
+// add comments
+//creates subclass for Parse Object
+var Comment = Parse.Object.extend("Comment");
+// Create a new instance of that class.
+var c = new Comment();
+//saves the fields and value
+c.save({
+	comment: document.getElementById("newComment").value ,
+	EventId: EventID ,
+	}, {
+  		success: function(c) {
+			
+			$("#commentslist").append("<li id="+String(c.id)+"><h3>"+ c.get("comment")+"</h3><p>"+ "" +"</p><p class="+"ui-li-aside"+">"+ c.createdAt +"</p></li>");
+		
+	
+		$("#commentslist").listview("refresh");
+    			//it worked, do nothing
+  	},
+		error: function(e, error) {
+			alert("there was an error");
+	}
+	});
 
+  },
+  error: function(E, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and description.
+	alert("there was an error retreiving object");
+  }
+});
 
 }
+
+/*
+$(document).delegate('#eventDetails', 'pageshow', function () {
+    //Your code for each page load here
+	alert("retreive comment");
+});*/
